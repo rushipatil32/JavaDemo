2c473ebcccef60429a103415b84ef7a780271418
Rushikesh Patil <rushipatil6632@gmail.com>
2022-02-03 11:59:28 +0530

    Added: Addition of two variables

---
+++ src/com/helloworld/java/Additon.java
@@ -0,0 +1,10 @@
+package com.helloworld.java;
+
+public class Additon {
+	public static void main(String[] args) {
+		int a = 1;
+		int b = 2;
+		int sum = a+b;
+		System.out.println(sum+"sum");
+	}
+}
